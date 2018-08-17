a5610fa78651a775d88ce53df29333504d76d93c
Todd <myoung44@vols.utk.edu>
2018-08-17 11:34:12 -0400

    pause

--- keras/cnn.py
+++ keras/cnn.py
@@ -40,18 +40,16 @@ def main():
     y_train = keras.utils.to_categorical(y_train, num_classes)
     y_test = keras.utils.to_categorical(y_test, num_classes)
 
-    kernel1, kernel2, dropout1, dropout2 = hparams
-
     model = Sequential()
-    model.add(Conv2D(32, kernel_size=(kernel1, kernel1),
+    model.add(Conv2D(32, kernel_size=(3, 3),
                      activation='relu',
                      input_shape=input_shape))
-    model.add(Conv2D(64, (kernel2, kernel2), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(dropout1))
+    model.add(Dropout(0.25))
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
-    model.add(Dropout(dropout2))
+    model.add(Dropout(0.25))
     model.add(Dense(num_classes, activation='softmax'))
 
     model.compile(loss=keras.losses.categorical_crossentropy,
@@ -61,7 +59,7 @@ def main():
     model.fit(x_train, y_train,
               batch_size=batch_size,
               epochs=epochs,
-              verbose=0,
+              verbose=1,
               validation_data=(x_test, y_test))
 
     score = model.evaluate(x_test, y_test, verbose=0)
